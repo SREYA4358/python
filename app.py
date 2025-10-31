f537613dd18ab6d4db674a842b482dc30adae722
SREYA4358 <sreyapunna4358@gmail.com>
2025-10-31 14:27:17 +0530

    added conditions

---
+++ app.py
@@ -0,0 +1,35 @@
+# print("hello world")
+# #primitive
+# a = 15 #integer
+# b = "24"#string
+# c = True#boolean
+# #non primitive
+# e = (2,3,5)#tuple
+# f = [24,15]#list
+# print(type(f))
+# j = {15,24,5}#set
+# print( type(j))
+# h = {id:'24'}#dict
+# print(type(h))
+# e = (2,3,5)
+# print(e [2])
+# e = [2,3,5]
+# e[1]=15
+# print(e)
+# def app():
+#     a = 15
+#     b = 30
+#     return print(a%b)
+# app()
+# age = int(input("enter your age"))
+# if(age>=18):
+#     print("eligible for vote")
+# else:
+#     print("not eligible to vote")    
+temp = 44
+if(temp>36):
+    print("temperature is very hot")
+elif(temp<=35):
+    print("temperature is warm")    
+else:
+    print("temperature is cold")   
